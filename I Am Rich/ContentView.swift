65de04972cebf3622688eb4e362d521a3f1879ac
letJamie <stillbut@naver.com>
2020-07-23 22:44:01 +0900

    Test swiftUI

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            Color(.blue)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
     }
 }
 
